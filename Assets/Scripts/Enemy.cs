b3ba570cacfd34876a3aad78c8ad99bc1847afcc
gabi <20gabriellah@gmail.com>
2025-04-28 13:37:12 -0400

    gr
    grgrgrgrgrgrgr

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,22 +7,22 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemyRB.velocity = Vector2.left * speed;
+    //    enemyRB.velocity = Vector2.left * speed;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        enemyRB.velocity = Vector2.left * speed;
+     //   enemyRB.velocity = Vector2.left * speed;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.transform.tag == "Bullet")
+       // if(other.transform.tag == "Bullet")
         {
-            gameObject.SetActive(false);
-            Destroy(other.gameObject);
-            Debug.Log("you got hit!!")
+         //   gameObject.SetActive(false);
+       //     Destroy(other.gameObject);
+        //    Debug.Log("you got hit!!");
         }
     }
 }
